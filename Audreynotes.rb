8379589b90e2cb403016e958efd551be6396ac30
Dreedle <davis.audrey.lynn@gmail.com>
2015-10-13 16:10:36 -0700

    Set up file structure

---
+++ Audreynotes.rb
@@ -0,0 +1,10 @@
+module Scrabble
+  class ScrabbleGame
+    letter_scores = {#a hash pairing each letter with each score}
+
+    self.score(word) #person who types out the hash may as well write this method, since they'll be using that hash
+
+    self.highest_score_from(array_of_words)
+  end
+
+end
