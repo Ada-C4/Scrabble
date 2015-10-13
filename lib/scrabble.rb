4b38a92976dbb5c182608640dceef1eb4725d7d5
noglows <jessica.noglows@outlook.com>
2015-10-13 16:20:22 -0700

    Score returns numberical score

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -11,7 +11,27 @@
 
 class Scrabble
   def score(word)
-    return 5
+    @score = 0
+    word.length.times do |index|
+      case word[index].upcase
+      when "A","E","I","O","U","L","N","R","S","T"
+        @score += 1
+      when "D","G"
+        @score += 2
+      when "B","C","M","P"
+        @score += 3
+      when "F","H","V","W","Y"
+        @score += 4
+      when "K"
+        @score += 5
+      when "J", "X"
+        @score += 8
+      when "Q", "Z"
+        @score += 10
+      end
+    end
+    return @score
+      #word[index]
   end
 
   def highest_score_from(array_of_words)
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -4,11 +4,13 @@ describe Scrabble do
   before :each do
     @scrabble = Scrabble.new
   end
+
   describe "#new" do
     it "create a new instance of Scrabble" do
       expect(@scrabble).to be_an_instance_of Scrabble
     end
   end
+
   describe "#score" do
     before :each do
       @guess_word = "rabbit"
@@ -17,8 +19,9 @@ describe Scrabble do
     it "returns a numeric score" do
       expect(@scrabble.score(@guess_word)).to be_a Fixnum
     end
+
     it "accepts strings as input" do
-      expect(@scrabble.score(nil)).
+      #expect(@scrabble.score(nil))
     end
   end
 end
