d6a1bd6afd6452aabe502686b17b52c494a975d0
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-10-13 16:29:44 -0700

    Adding initial setup and first two specs

---
+++ lib/scrabble.rb
@@ -0,0 +1,20 @@
+module Scrabble
+
+  SCORES = {
+    a: 1,
+    b: 3
+  }
+
+  class Scrabble
+    def self.score(word)
+      #create variable for word score
+      #split word into array of characters - downcased
+      #convert letters to symbols
+      #look up score for each symbol and add it to word score variable
+      #return word score variable
+    end
+
+    def self.highest_score_from(array_of_words)
+    end
+  end
+end
---
+++ spec/scrabble_spec.rb
@@ -0,0 +1,23 @@
+require "./lib/scrabble"
+
+describe Scrabble do
+  before :each do
+    @word = "abba"
+    @number = 93
+  end
+  describe "#self.score" do
+    it "returns an integer" do
+      expect(Scrabble::Scrabble.score(@word)).to be_kind_of(Fixnum)
+    end
+    it "accepts only a string" do
+      expect(Scrabble::Scrabble.score(@word)).to be_kind_of(Fixnum)
+      expect(Scrabble::Scrabble.score(@number)).to eq("Error message")
+    end
+  end
+end
+
+describe "SCORES" do
+  it "matches keys with values" do
+    expect(Scrabble::SCORES[:a]).to eq 1
+  end
+end
