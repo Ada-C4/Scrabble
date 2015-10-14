ff26487bd3e4ad851a9cff7aaf6f3fcb8120afa8
noglows <jessica.noglows@outlook.com>
2015-10-14 14:35:30 -0700

    Starting Wave 2

---
+++ lib/player.rb
@@ -0,0 +1,12 @@
+class Player
+  attr_accessor :name
+
+  def initialize
+    @name = name
+  end
+
+  # def self.new(name)
+  #   @name = name
+  # end
+
+end
---
+++ spec/player_spec.rb
@@ -0,0 +1,22 @@
+require './lib/player.rb'
+
+describe Player do
+  before :each do
+    @player = Player.new()
+    #@player.new("Ada")
+  end
+
+  describe "#new" do
+    it "create a new instance of Player" do
+      expect(@player).to be_an_instance_of Player
+    end
+
+    # it "assigns the input name to the @name variable" do
+    #   expect(Player.name).to eq "Ada"
+    # end
+
+  end
+
+
+
+end
