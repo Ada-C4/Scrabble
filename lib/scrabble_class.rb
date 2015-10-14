6ae140b2fdd0203f7761171ab5e2ae265ddb82a2
Rebecca Tolmach <rmtolmach@gmail.com>
2015-10-14 15:28:09 -0700

    some passing and some failing tests in Wave 1

--- lib/scrabble_class.rb
+++ lib/scrabble_class.rb
@@ -2,17 +2,17 @@ module Scrabble
   class Scrabble
     def self.letter_score(letter)
       case letter
-        when "A" || "E" || "I" || "O" || "U" || "L" || "N" || "R" || "S" || "T"
+       when "A", "E", "I", "O", "U", "L", "N", "R", "S", "T"
          point = 1
-        when "D" || "G"
+       when "D", "G"
          point = 2
-        when "B" || "C" || "M" || "P"
+       when "B", "C", "M", "P"
          point = 3
-        when "F" || "H" || "V" || "W" || "Y"
+       when "F", "H", "V", "W", "Y"
          point = 4
-        when "K"
+       when "K"
          point = 5
-        when "J" || "X"
+       when "J", "X"
          point = 8
         else
          point = 10
@@ -20,10 +20,32 @@ module Scrabble
       return point
     end
 
-  def self.letter_array(word)
+  def self.score(word)
     l_array = word.split("")
-    return l_array
+    @sum = 0
+    l_array.each do |l|
+      score = letter_score(l)
+      @sum += score
+    end
+    if l_array.count == 7
+      @sum = @sum + 50
+    end
+    return @sum
   end
 
+
+
+  def self.highest_score_from(array_of_words)
+    max_score = 0
+    max_word = ""
+    array_of_words.each do |word|
+      score = self.score(word)
+    if score > max_score
+      max_score = score
+      max_word = word
+    end
+    return max_word
+    end
+  end
   end
 end
--- spec/scrabble_class_spec.rb
+++ spec/scrabble_class_spec.rb
@@ -1,12 +1,30 @@
 require './lib/scrabble_class.rb'
 
 describe Scrabble do
-  before :each do
-    @game = Scrabble.new
-  end
-  describe ".new" do
-    it "creates a new instance of a Chair" do
-      expect(@game).to be_an_instance_of Scrabble
+  describe ".letter_score" do
+    it "should be three points" do
+      expect(Scrabble::Scrabble.letter_score("M")).to eq 3
+    end
+    it "should be 8 for peanut" do
+      expect(Scrabble::Scrabble.score("PEANUT")).to eq 8
+    end
+    it "should give 10 points for Z" do
+      expect(Scrabble::Scrabble.score("Z")).to eq 10
+    end
+    it "gives a 50 point bonus for using 7 letters" do
+      expect(Scrabble::Scrabble.score("QUANTUM")).to eq 68
+    end
+    it "returns QUIZ" do
+      expect(Scrabble::Scrabble.highest_score_from(["QUIZ", "CAT"])).to eq "QUIZ"
+    end
+    it "returns ZQZQZQ" do
+      expect(Scrabble::Scrabble.highest_score_from(["LOLOLOL", "ZQZQZQ"])).to eq "ZQZQZQ"
+    end
+    it "returns LOLOLOG" do # this is passing without writing any specific code
+      expect(Scrabble::Scrabble.highest_score_from(["ZZZZZX", "LOLOLOG"])).to eq "LOLOLOG"
+    end
+    it "returns ZAM" do
+      expect(Scrabble::Scrabble.highest_score_from(["ELBOWW", "ZAM"])).to eq "ZAM"
     end
   end
 end
