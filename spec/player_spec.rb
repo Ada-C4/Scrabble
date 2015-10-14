2412813a458dca6c33b63585963b4869da5395f0
Desiree <desireepoland@gmail.com>
2015-10-14 15:03:32 -0700

    add spec for Scrabble::Player#plays

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -10,4 +10,17 @@ describe Scrabble::Player do
       expect(@player1).to be_an_instance_of Scrabble::Player
     end
   end
+
+
+  describe "#name" do
+    it "returns the @name variable" do
+      expect(@player1.name).to eq "bob"
+    end
+  end
+  
+  describe "#plays" do
+    it "returns an array of the words played by player" do
+      expect(@player1.plays).to eq []
+    end
+  end
 end
