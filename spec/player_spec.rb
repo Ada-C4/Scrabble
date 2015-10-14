c23e38e28e806b717be87c593fb6ea3858f2cd58
Jenna Nichols <jennaplusplus@gmail.com>
2015-10-14 14:30:43 -0700

    fixed naming conventions in spec files

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,14 +2,14 @@ require "./lib/player.rb"
 require "./lib/scrabble.rb"
 
 describe Scrabble do
-  describe Player do
+  describe Scrabble::Player do
     before :each do
       @player = Scrabble::Player.new("Jenn")
     end
 
     describe ".new(name)" do
       it "creates a new instance of Player with a name" do
-        expect(@player).to be_an_instance_of Player
+        expect(@player).to be_an_instance_of Scrabble::Player
       end
     end
 
