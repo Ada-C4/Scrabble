b8c98da18412c8082d476136a2b8b0025849c119
Dreedle <davis.audrey.lynn@gmail.com>
2015-10-14 15:07:43 -0700

    highest score can return a hash of top scoring words

--- lib/highest_score.rb
+++ lib/highest_score.rb
@@ -1,23 +1,30 @@
-require "./score.rb"
+require "./lib/score.rb"
 
 module Scrabble
   class ScrabbleGame
-    # letter_scores = {#a hash pairing each letter with each score}
 
-    # self.score(word) #person who types out the hash may as well write this method, since they'll be using that hash
-    #Sarah T is writing the above
     def self.highest_score_from(array_of_words)
-      #for each word in array, call Sarah's method to get the score
-      def
-        wordscore_array = []
+      #for each word in array, call method to get the score
+      #and make a hash of words with scores
+        wordscore_hash = {}
         array_of_words.each do |w|
           score = self.score(w)
-          wordscore_array.push(score)
+          wordscore_hash[w] = score
         end
-        return wordscore_array
-      end
-      
+
+        #find max value of a value in a hash
+        # wordscore_hash.max[0]
+
+        #returns all keys with max value
+        top_scoring_words_hash = {}
+        wordscore_hash.each do |k, v|
+          if v == wordscore_hash.values.max
+            top_scoring_words_hash[k] = v
+          end
+        end
+        top_scoring_words_hash
+
+
     end
   end
-
 end
--- spec/highest_score_spec.rb
+++ spec/highest_score_spec.rb
@@ -5,19 +5,32 @@ describe Scrabble::ScrabbleGame do
   describe "self.highest_score_from" do
     before :each do
       @word_array1 = ["aaa", "bbb"]
-      @word_array2 = ["puppy", "huzzah", "cat", "baritone", "zxzx", "asteroid"]
+      @word_array2 = ["bb", "aaaaaa", "a", "b", "ki"]
+      @word_array3 = ["puppy", "huzzah", "cat", "baritone", "zxzx", "asteroid"]
     end
 
     it "Can look at this method with rspec" do
       puts "Rspec can see in here"
     end
 
-    it "Rspec can see method" do
-      expect(Scrabble::ScrabbleGame.highest_score_from("hi")).to eq "hi"
+    # it "Rspec can see method" do
+    #   expect(Scrabble::ScrabbleGame.highest_score_from("hi")).to eq "hi"
+    # end
+
+    # it "Score each word in the array and return an array of scores" do
+    #   expect(Scrabble::ScrabbleGame.highest_score_from(@word_array1)).to eq [3, 9]
+    # end
+
+    it "Top_scoring_words_hash contains words that have tied scores" do
+      expect(Scrabble::ScrabbleGame.highest_score_from(@word_array2)).to eq ({"bb"=>6, "aaaaaa"=>6, "ki"=>6})
     end
 
-    it "Score each word in the array and return an array of scores" do
-      expect(Scrabble::ScrabbleGame.highest_score_from(@word_array1)).to eq [3, 9]
-    end
+    # it "Return word with highest score" do
+    #   expect(Scrabble::ScrabbleGame.highest_score_from(@word_array1)).to be == "bbb"
+    # end
+    #
+    # it "If more than one highest scoring word, go with the shorter word" do
+    #   expect(Scrabble::ScrabbleGame.highest_score_from(@word_array2)).to be == "bb"
+    # end
   end
 end
