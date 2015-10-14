7c65cd9134b7f0d43948c58018d8cb8c6022edd4
Dreedle <davis.audrey.lynn@gmail.com>
2015-10-14 13:26:23 -0700

    updated highest_score methods

--- lib/highest_score.rb
+++ lib/highest_score.rb
@@ -1,3 +1,5 @@
+require "./score.rb"
+
 module Scrabble
   class ScrabbleGame
     # letter_scores = {#a hash pairing each letter with each score}
@@ -6,7 +8,15 @@ module Scrabble
     #Sarah T is writing the above
     def self.highest_score_from(array_of_words)
       #for each word in array, call Sarah's method to get the score
-      return array_of_words
+      def
+        wordscore_array = []
+        array_of_words.each do |w|
+          score = self.score(w)
+          wordscore_array.push(score)
+        end
+        return wordscore_array
+      end
+      
     end
   end
 
--- spec/highest_score_spec.rb
+++ spec/highest_score_spec.rb
@@ -2,19 +2,10 @@ require "./lib/highest_score.rb"
 
 describe Scrabble::ScrabbleGame do
 
-  # before :each do
-  #   @scrabble = Scrabble::ScrabbleGame.new
-  # end
-
-  # describe ".new" do
-  #   it "creates a new instance of ScrabbleGame" do
-  #       expect(@scrabble).to be_an_instance_of Scrabble::ScrabbleGame
-  #   end
-  # end
-
   describe "self.highest_score_from" do
     before :each do
-      @word_array = ["puppy", "huzzah", "cat", "baritone", "zxzx", "asteroid"]
+      @word_array1 = ["aaa", "bbb"]
+      @word_array2 = ["puppy", "huzzah", "cat", "baritone", "zxzx", "asteroid"]
     end
 
     it "Can look at this method with rspec" do
@@ -24,13 +15,9 @@ describe Scrabble::ScrabbleGame do
     it "Rspec can see method" do
       expect(Scrabble::ScrabbleGame.highest_score_from("hi")).to eq "hi"
     end
-    
-    # it "Array is an array of strings" do
-    #
-    # end
 
-    #it "For each word in array, call method to get the score" do
-    #
-    #end
+    it "Score each word in the array and return an array of scores" do
+      expect(Scrabble::ScrabbleGame.highest_score_from(@word_array1)).to eq [3, 9]
+    end
   end
 end
