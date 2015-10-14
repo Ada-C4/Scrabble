e15889fed4a4dc18f79d775de7dfd0d27b24cccf
Kelly <kelly_devlin@alumni.brown.edu>
2015-10-14 13:57:32 -0700

    wave 1 - refactor

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,16 +1,10 @@
 module Scrabble
   class Scrabble
     LETTER_VALUES = {A: 1, E: 1, I: 1, O: 1,  U: 1, L: 1, N: 1, R: 1, S: 1, T: 1, D:  2, G: 2, B: 3, C: 3, M: 3, P: 3, F: 4,  H: 4, V: 4, W: 4, Y: 4, K: 5, J: 8, X:  8, Q: 10, Z: 10}
-    def self.return_letter_value(letter)
-      LETTER_VALUES[letter.upcase.to_sym]
-    end
-    def self.make_array(word)
-      return word.each_char.to_a
-    end
     def self.make_value_array(word)
-      word_array = self.make_array(word)
+      word_array = word.each_char.to_a
       word_array.map do |letter|
-         self.return_letter_value(letter)
+         LETTER_VALUES[letter.upcase.to_sym]
       end
     end
     def self.score(word)
@@ -32,18 +26,16 @@ module Scrabble
       end
       return word_score_hash
     end
-    def self.highest_score_value(array_of_words)
-      pair = self.create_word_score_hash(array_of_words).max_by do |k, v|
-        v
-      end
-      return pair[1]
-    end
     def self.highest_score_from(array_of_words)
+      highest_pair = self.create_word_score_hash(array_of_words).max_by do |k, v|
+          v
+      end
+      highest_score_value = highest_pair[1]
       highest_score_hash = self.create_word_score_hash(array_of_words).select do |k, v|
-        v == self.highest_score_value(array_of_words)
+        v == highest_score_value
       end
       if highest_score_hash.length == 1
-        return highest_score_hash.key(self.highest_score_value(array_of_words))
+        return highest_score_hash.key(highest_score_value)
       else
         pair = highest_score_hash.min_by do |k, v|
           k.length
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -15,16 +15,6 @@ describe Scrabble::Scrabble do
       expect(@game).to be_an_instance_of Scrabble::Scrabble
     end
   end
-  describe ".return_letter_value" do
-    it "returns value for letter using LETTER_VALUES" do
-      expect(Scrabble::Scrabble.return_letter_value(@letter)).to eq 1
-    end
-  end
-  describe ".self.make_array(word)" do
-    it "creates an array of letters in the word" do
-      expect(Scrabble::Scrabble.make_array(@word)).to eq ["c", "a", "t"]
-    end
-  end
   describe ".self.make_value_array(word)" do
     it "returns an array of letter values" do
       expect(Scrabble::Scrabble.make_value_array(@word)).to eq [3, 1, 1]
@@ -41,11 +31,6 @@ describe Scrabble::Scrabble do
   describe "self.create_word_score_hash(array_of_words)" do
     it "returns a hash of word: value pairs" do
       expect(Scrabble::Scrabble.create_word_score_hash(@array_of_words)).to eq @hash
-    end
-  end
-  describe "self.highest_score_value(array_of_words)" do
-    it "returns highest score value" do
-      expect(Scrabble::Scrabble.highest_score_value(@array_of_words)).to eq 16
     end
   end
   describe "self.highest_score_from(array_of_words)" do
