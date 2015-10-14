000feb30acc9ba3af9c3ac6ff193315eeea9b277
Wesley Willis <haileyw@gmail.com>
2015-10-14 11:31:13 -0700

    separate word score and letter score methods- add spec tests for each

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,9 +1,19 @@
 class Scrabble
-  def word_score(letter)
+  def letter_score(letter)
     case letter
-    when "m", "b", "c"
+    when "a" #E, I, O, U, L, N, R, S, T
+      return 1
+    when "m", "b", "c", "p"
       return 3
-
     end
+  end
+  #SCORE = 0
+  def word_score(word)
+    score = 0
+    word.split("").each do |letter|
+      letter = letter_score(letter)
+      score += letter
+    end
+    return score
   end
 end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -8,17 +8,35 @@ describe Scrabble do
     expect(@scrabble).to be_an_instance_of Scrabble
   end
 
-  describe "#word_score" do
+  describe "#letter_score" do
+    it "scores the letter a" do
+      expect(@scrabble.letter_score("a")).to eq 1
+    end
+
+    #it "scores the letter e" do
+    #  expect(@scrabble.letter_score("e")).to eq 1
+    #end
+
     it "scores the letter m" do
-      expect(@scrabble.word_score("m")).to eq 3
+      expect(@scrabble.letter_score("m")).to eq 3
     end
 
     it "scores the letter b" do
-      expect(@scrabble.word_score("b")).to eq 3
+      expect(@scrabble.letter_score("b")).to eq 3
     end
 
     it "scores the letter c" do
-      expect(@scrabble.word_score("c")).to eq 3
+      expect(@scrabble.letter_score("c")).to eq 3
+    end
+
+    it "scores the letter p" do
+      expect(@scrabble.letter_score("p")).to eq 3
+    end
+  end
+
+  describe "#word_score" do
+    it "iterates through letters and changes them to integers" do
+      expect(@scrabble.word_score("m" "a")).to eq 4
     end
   end
 end
