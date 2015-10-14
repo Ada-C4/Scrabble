0624faa4b0e7d549eaab872ed80be5928f7f1e26
Ricky Hougland <houglande@gmail.com>
2015-10-14 10:06:56 -0700

    Finished adding 8 specs

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -3,30 +3,30 @@ module Scrabble
   SCORES = {
     a: 1,
     b: 3,
-    c: ,
-    d: ,
-    e: ,
-    f: ,
-    g: ,
-    h: ,
-    i: ,
-    j: ,
-    k: ,
-    l: ,
-    m: ,
-    n: ,
-    o: ,
-    p: ,
-    q: ,
-    r: ,
-    s: ,
-    t: ,
-    u: ,
-    v: ,
-    w: ,
-    x: ,
-    y: ,
-    z:
+  #   c: ,
+  #   d: ,
+  #   e: ,
+  #   f: ,
+  #   g: ,
+  #   h: ,
+  #   i: ,
+  #   j: ,
+  #   k: ,
+  #   l: ,
+  #   m: ,
+  #   n: ,
+  #   o: ,
+  #   p: ,
+  #   q: ,
+  #   r: ,
+  #   s: ,
+  #   t: ,
+  #   u: ,
+  #   v: ,
+  #   w: ,
+  #   x: ,
+  #   y: ,
+  #   z:
   }
 
   class Scrabble
@@ -50,19 +50,23 @@ module Scrabble
     end
 
     def self.highest_score_from(array_of_words)
-      #sanitize input
-      #run the self.score method on each word in the array
-      #store the word and the score in a hash (or maybe an array)
       word_hash = {}
-      array_of_words.each do |word|
-        word_hash[word] = Scrabble::Scrabble.score(word)
-      end
-      #retrieve the highest scoring word
-      word_hash.each do |k,v|
-        return k if v == word_hash.values.max
+
+      #sanitize input
+      if array_of_words.is_a? Array
+        #run the self.score method on each word in the array
+        #store the word and the score in a hash (or maybe an array)
+        array_of_words.each do |word|
+          word_hash[word] = Scrabble::Scrabble.score(word)
+        end
+        #retrieve the highest scoring word
+        word_hash.each do |k,v|
+          return k if v == word_hash.values.max
+        end
+      else
+        return "Error message"
       end
     end
-
   end
 
 end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -6,8 +6,11 @@ describe Scrabble do
     @number = 93
     @float = 1.5
     @array = [1,2,3]
-    @array_of_words = ["ab","baaaa"]
+    @array_of_words = ["ab","baaaaaaa", "aabbaa"]
+    @array_of_words2 = ["aaaaaaa", "abb"]
+    @array_of_words3 = ["ab", "abb", "bba"]
   end
+
   describe "#self.score" do
     it "returns an integer" do
       expect(Scrabble::Scrabble.score(@word)).to be_kind_of(Fixnum)
@@ -18,11 +21,25 @@ describe Scrabble do
       expect(Scrabble::Scrabble.score(@float)).to eq "Error message"
       expect(Scrabble::Scrabble.score(@array)).to eq "Error message"
     end
+    it "correctly scores a word" do
+      expect(Scrabble::Scrabble.score(@word)).to eq 8
+    end
   end
+
   describe "#self.highest_score_from" do
     it "returns a string" do
       expect(Scrabble::Scrabble.highest_score_from(@array_of_words)).to be_kind_of(String)
     end
+    it "selects word with 7 letters in case of tie" do
+      expect(Scrabble::Scrabble.highest_score_from(@array_of_words2)).to eq "aaaaaaa"
+    end
+    it "selects word with fewest letters in case of tie, unless one of words has 7 letters" do
+      expect(Scrabble::Scrabble.highest_score_from(@array_of_words)).to eq "aabbaa"
+    end
+    it "in case of score and word length tie, selects first word provided" do
+      expect(Scrabble::Scrabble.highest_score_from(@array_of_words3)).to eq "abb"
+    end
+
   end
 end
 
