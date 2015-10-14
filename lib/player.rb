0034b220397baa8dfdd41dfcdc72bc662a35f818
emgord <emgord@gmail.com>
2015-10-14 14:53:00 -0700

    added spec and method for plays array and play(word) method

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,14 @@
 module Scrabble
   class Player
-    attr_reader :name
+    attr_reader :name, :plays
 
     def initialize(name)
-      @name = name 
+      @name = name
+      @plays = []
+    end
+
+    def play(word)
+      @plays.push(word)
     end
 
   end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -8,8 +8,20 @@ describe Scrabble::Player do
     it "creates a new instance of player" do
       expect(@player).to be_an_instance_of Scrabble::Player
     end
-    it "creates a new instance of player with name ergdarg" do
+  end
+  describe "#name" do
+    it "gives the name of the player" do
       expect(@player.name).to eq "ergdarg"
+    end
+  end
+  describe "#plays" do
+    it "returns an empty array of words at the beginning of the game" do
+      expect(@player.plays).to eq []
+    end
+  end
+  describe "#play(word)" do
+    it "adds the played word to the @plays array" do
+      expect(@player.play("word")).to eq ["word"]
     end
   end
 end
