d21e25f51e939ac3d1d6d964e61e348ff920dcab
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-10-14 09:46:03 -0700

    Built out letter array

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -2,7 +2,31 @@ module Scrabble
 
   SCORES = {
     a: 1,
-    b: 3
+    b: 3,
+    c: ,
+    d: ,
+    e: ,
+    f: ,
+    g: ,
+    h: ,
+    i: ,
+    j: ,
+    k: ,
+    l: ,
+    m: ,
+    n: ,
+    o: ,
+    p: ,
+    q: ,
+    r: ,
+    s: ,
+    t: ,
+    u: ,
+    v: ,
+    w: ,
+    x: ,
+    y: ,
+    z:
   }
 
   class Scrabble
