dae504f189996107a9183b5b902e2f71774adc3b
Desiree <desireepoland@gmail.com>
2015-10-14 15:57:24 -0700

    Add total_score spec and method

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,22 @@
 module Scrabble
   class Player
-    attr_reader :name, :plays
+    attr_reader :name, :plays, :total
     def initialize(name)
       @name = name
       @plays = []
+      @total = 0
     end
+
+    def play(word)
+      @plays.push(word)
+    end
+
+    def total_score
+      @plays.each do |word|
+        @total += Scrabble.score(word)
+      end
+      return @total
+    end
+
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,7 +2,12 @@ require "./lib/player"
 
 describe Scrabble::Player do
   before :each do
-    @player1 = Scrabble::Player.new "bob"
+    @player1 = Scrabble::Player.new("bob")
+    @player2 = Scrabble::Player.new("andre")
+
+    @player2.play("love") # love = 7
+    @player2.play("toast") # toast = 5
+    @player2.play("hand") # hand = 8
   end
 
   describe ".new" do
@@ -20,6 +25,25 @@ describe Scrabble::Player do
   describe "#plays" do
     it "returns an array of the words played by player" do
       expect(@player1.plays).to eq []
+    end
+  end
+
+  describe "#play" do
+    it "adds the input word to the plays array" do
+      @player1.play("sparkle")
+      expect(@player1.plays).to eq ["sparkle"]
+    end
+    it "adds the input word to the plays array" do
+      expect(@player2.plays).to eq ["love", "toast", "hand"]
+    end
+    # it "returns false is player has already won" do
+    #   expect(@player3.plays).to eq false
+    # end
+  end
+
+  describe "#total_score" do
+    it "sums up and returns the score of the players words" do
+      expect(@player2.total_score).to eq 20
     end
   end
 
