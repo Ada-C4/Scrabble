5aad2823db6a19b13d0c595fa9f16f2e3b27b7ba
Jennie Buechner <janina.buechner@gmail.com>
2015-10-15 16:17:36 -0700

    add test for draw_tiles method

--- spec/tilebag_spec.rb
+++ spec/tilebag_spec.rb
@@ -7,15 +7,26 @@ describe Scrabble do
     before :each do
       @tilebag = Scrabble::TileBag.new
     end
-    it "sets up a new tile bag with a collection of tiles" do
-      expect(@tilebag.tiles.length).to eq(98)
+
+    describe "#initialize" do
+      it "sets up a new tile bag with a collection of tiles" do
+        expect(@tilebag.tiles.length).to eq(98)
+      end
+
+      it "has the correct quantity of tiles for each letter" do
+        expect(@tilebag.tiles.count("a")).to eq(9)
+        expect(@tilebag.tiles.count("k")).to eq(1)
+        expect(@tilebag.tiles.count("1")).to eq(0)
+        expect(@tilebag.tiles.count("r")).to eq(6)
+      end
     end
 
-    it "has the correct quantity of tiles for each letter" do
-      expect(@tilebag.tiles.count("a")).to eq(9)
-      expect(@tilebag.tiles.count("k")).to eq(1)
-      expect(@tilebag.tiles.count("1")).to eq(0)
-      expect(@tilebag.tiles.count("r")).to eq(6)
+    describe "#draw_tiles(num)" do
+      it "draws the correct number of tiles from the bag" do
+        start = @tilebag.tiles.length
+        @tilebag.draw_tiles(5)
+        expect(@tilebag.tiles.length).to eq(start-5)
+      end
     end
 
 
