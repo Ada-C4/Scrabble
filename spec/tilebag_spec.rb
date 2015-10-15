7cd92cf9ae47122092a0ff397f1f864b7eca6458
emgord <emgord@gmail.com>
2015-10-15 16:10:52 -0700

    added method to create tilebag and test it has the right number of tiles

--- spec/tilebag_spec.rb
+++ spec/tilebag_spec.rb
@@ -1,1 +1,18 @@
 require "./lib/scrabble_master"
+
+describe Scrabble::TileBag do
+  before :each do
+    @tilebag = Scrabble::TileBag.new
+  end
+  describe ".new" do
+    it "creates a new instance of TileBag using the initialize method" do
+      expect(@tilebag).to be_an_instance_of Scrabble::TileBag
+    end
+  end
+  describe "#initialize" do
+    it "creates a new tile bag full of 98 tiles" do
+      expect(@tilebag.tiles.length).to eq 98
+    end
+  end
+
+end
