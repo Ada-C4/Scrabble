16675c1cb30a665844a1e81be5f2e842197b90cd
Jennie Buechner <janina.buechner@gmail.com>
2015-10-15 16:24:02 -0700

    add more tests for draw_tiles method

--- spec/tilebag_spec.rb
+++ spec/tilebag_spec.rb
@@ -27,6 +27,17 @@ describe Scrabble do
         @tilebag.draw_tiles(5)
         expect(@tilebag.tiles.length).to eq(start-5)
       end
+
+      it "returns an array of letters of num length" do
+        letters_drawn = @tilebag.draw_tiles(5)
+        expect(letters_drawn).to be(Array)
+        expect(letters_drawn.length).to eq(5)
+      end
+
+      it "returns an error if num is not between 0 and 7" do
+        expect(@tilebag.draw_tiles(-1)).to be([])
+        expect(@tilebag.draw_tiles(10)).to be([])
+      end
     end
 
 
