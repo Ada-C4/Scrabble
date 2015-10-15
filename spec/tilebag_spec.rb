d775463ad3b7ecbb3777a769cc7f14b6406f0478
Kelly <kelly_devlin@alumni.brown.edu>
2015-10-15 16:12:15 -0700

    created TileBag class and initialize method

---
+++ spec/tilebag_spec.rb
@@ -0,0 +1,24 @@
+require "./lib/TileBag"
+describe Scrabble::TileBag do
+  before :each do
+    @tilebag = Scrabble::TileBag.new
+  end
+  describe ".self.new" do
+    it "creates a new instance" do
+      expect(@tilebag).to be_an_instance_of Scrabble::TileBag
+    end
+  end
+  describe "#initialize" do
+    it "sets up an instance with a collection of default tiles" do
+      expect(@tilebag.tile_quantities).to eq Scrabble::DEFAULT_TILES
+    end
+  end
+  describe "#draw_tiles(num)" do
+    it "returns num number of random tiles" do
+      expect(@tilebag.draw_tiles(4).length).to eq 4
+    end
+    it "removes the tiles from the default set" do
+
+    end
+  end
+end
