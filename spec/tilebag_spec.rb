215be957d58e28e9d7a8262b3f61189e8525dcde
Desiree <desireepoland@gmail.com>
2015-10-16 10:21:03 -0700

    Add code for draw_tiles(num) and test and code for tiles_remaining

--- spec/tilebag_spec.rb
+++ spec/tilebag_spec.rb
@@ -12,17 +12,23 @@ describe Scrabble::TileBag do
     it "sets up an instance with a collection of default tiles" do
       expect(@tilebag1.tile_quantities).to_not be_empty
       expect(@tilebag1.tile_quantities.length).to eq 98
-      #test length to eq 100
-      #expect(@tilebag1.tile_quantities["W"]).to eq 2
-      #expect(@tilebag1.tile_quantities["L"]).to eq 4
     end
   end
 
-  # describe "#draw_tiles" do
-  #   it "returns the specifed number of random tiles" do
-  #     expect(@tilebag1.draw_tiles(4).length).to eq 4
-  #   end
-  # end
+  describe "#draw_tiles" do
+    it "returns the specifed number of random tiles" do
+      expect(@tilebag1.draw_tiles(4).length).to eq 4
+    end
+    it "removes the tiles from the default set" do
+      expect(@tilebag1.tile_quantities.length).to eq 94
+    end
+  end
+
+  describe "#tiles_remaining" do
+    it " returns the number of tiles remaining in the bag" do
+      expect(@tilebag1.tiles_remaining).to eq 94
+    end
+  end
 
 
 end
