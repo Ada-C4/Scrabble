46a8e096c8da19368bb6909c7e36066e354c7418
emgord <emgord@gmail.com>
2015-10-16 15:05:39 -0700

    added test of incorrect word to dictionary

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -4,4 +4,7 @@ describe ".lookup(word)" do
   it "accepts a word from the dictionary" do
     expect(Scrabble::WordDictionary.lookup("word")).to be_truthy
   end
+  it "rejects a word that is not in the dictionary" do
+    expect(Scrabble::WordDictionary.lookup("zzzz")).to be_falsey
+  end
 end
