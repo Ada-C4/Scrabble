f1794ca4d7e55f62ee86ad1ff57fbe4cfe3b919b
Wesley Willis <haileyw@gmail.com>
2015-10-16 15:38:34 -0700

    added new subtract tiles method

--- lib/tile_bag.rb
+++ lib/tile_bag.rb
@@ -3,17 +3,25 @@ require "./lib/scrabble"
 class TileBag < Scrabble::Scrabble
   def initialize
     @bag = ["b", "b", "c", "c", "f", "f", "p", "p", "v", "v"]
-    @tiles = []
+    @dealt_tiles = []
     @default_tiles = draw_tiles(7)
   end
 
 
   def draw_tiles(num)
-  tiles = @tiles
+  tiles = @dealt_tiles
     num.times do
-      tiles.push(@bag.sample)
+      t = @bag.sample
+      tiles.push(t)
+      remove_tiles(t)
     end
     tiles
+  end
+
+  def remove_tiles(letter)
+    spot = @bag.index(letter)
+    @bag.delete_at(spot)
+
   end
 end
 
--- spec/tile_bag_spec.rb
+++ spec/tile_bag_spec.rb
@@ -7,7 +7,6 @@ describe TileBag do
 
   describe '@default_tiles' do
     it "selects 7 random tiles" do
-      @default_tiles
       expect(@tile_bag).to eq ['b', "b", "a"]
     end
   end
@@ -15,6 +14,12 @@ describe TileBag do
   describe "gives us 3 tiles" do
     it "returns random tiles" do
       expect(@tile_bag.draw_tiles(3)).to eq ['b', "b", "a"]
+    end
+  end
+
+  describe "subtracts tiles from our bag" do
+    it "subtracts tiles" do
+      expect(@tile_bag).to eq ['b', "b", "a"]
     end
   end
 end
