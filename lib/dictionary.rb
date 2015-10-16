7472488372beff757d3795ef1b93093f8492573d
Jenna Nichols <jennaplusplus@gmail.com>
2015-10-16 13:31:58 -0700

    updated Dictionary class

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -11,19 +11,12 @@ module Scrabble
 
     def check_word(word)
       dict_file = File.open("./lib/puzzler_list.txt", "r")
-      good_word = 0
       dict_file.each_line do |line|
         if word  == line.strip
-          good_word += 1
+          return true
         end
       end
-
-      if good_word > 0
-        return true
-      else
-        return false
-      end
-
+      return false
     end
 
   end
