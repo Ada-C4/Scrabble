3e047463a5b565ebc938d19f8374ddd780bc8d8a
Lauren Granger <lr.granger21@gmail.com>
2015-10-16 11:07:22 -0700

    spec for tile_bag draw_tiles

--- spec/tilebag_class_spec.rb
+++ spec/tilebag_class_spec.rb
@@ -11,25 +11,30 @@ describe Scrabble::TileBag do
       expect(@bag).to be_an_instance_of Scrabble::TileBag
     end
     it "has 12 E's, 8 O's and 2 Y's" do
-        e_array = @bag.avaliable_tiles_array.find_all{ |l| l == "E"}
-        o_array = @bag.avaliable_tiles_array.find_all{ |l| l == "O"}
-        y_array = @bag.avaliable_tiles_array.find_all{ |l| l == "Y"}
+      e_array = @bag.avaliable_tiles_array.find_all{ |l| l == "E"}
+      o_array = @bag.avaliable_tiles_array.find_all{ |l| l == "O"}
+      y_array = @bag.avaliable_tiles_array.find_all{ |l| l == "Y"}
       expect(e_array.length).to eq 12
       expect(o_array.length).to eq 8
       expect(y_array.length).to eq 2
     end
   end
 
-#   desbribe "draw_tiles" do
-#     it "returns the number of tiles the player has asked for randomly from the bag tiles" do
-#       expect()
-#     end
-#     it "removes the tiles given to the player from the default set" do
-#       expect
-#     end
-#   end
+  describe "draw_tiles" do
+    before :each do
+      @bag = Scrabble::TileBag.new
+      @count = @bag.avaliable_tiles_array.length
+      @bag.draw_tiles(5)
+    end
+    it "returns the number of tiles the player has asked for randomly from the bag tiles" do
+      expect(@bag.player_tiles.length).to eq 5
+    end
+    it "removes the tiles given to the player from the default set" do
+      expect(@bag.avaliable_tiles_array.length).to eq (@count - 5)
+    end
+  end
 #
-#   desbribe "tiles_remaining" do
+#   describe "tiles_remaining" do
 #     it "returns the number of tiles remaining in the bag" do
 #       expect()
 #     end
