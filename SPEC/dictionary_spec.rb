27dc92c5d42d7339326ea980ffda477d85c02ac4
Brittany <BrittanyKohler@gmail.com>
2015-10-16 12:10:34 -0700

    adding additional test for dictionary valid_word? method

--- SPEC/dictionary_spec.rb
+++ SPEC/dictionary_spec.rb
@@ -5,5 +5,8 @@ describe Scrabble::Dictionary do
     it "checks to see if aardvark is a valid word (is included in the dictionary)" do
       expect(Scrabble::Dictionary.valid_word?("aardvark")).to eq true
     end
+    it "checks to see if dfskldfj is not a valid word (is not included in the dictionary)" do
+      expect(Scrabble::Dictionary.valid_word?("dfskldfj")).to eq false
+    end
   end
 end
