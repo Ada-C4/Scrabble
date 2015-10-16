b25ce4a1fdfa90c112be7ebe73a0579f0dd37f6d
Daphne <dg1181@nyu.edu>
2015-10-15 22:19:24 -0700

    Reworked how @tilebag is generated

--- lib/tilebag.rb
+++ lib/tilebag.rb
@@ -4,12 +4,9 @@ module Scrabble
     attr_reader :tile_bag
 
     def initialize
-      @tile_bag = create_new_tile_bag
-    end
-
-    def create_new_tile_bag
-      letter_string = "A"*9 + "B"*2 + "C"*2 + "D"*4 + "E"*12 + "F"*2 + "G"*3 + "H"*2 + "I"*9 + "J"*1 + "K"*1 + "L"*4 + "M"*2 + "N"*6 + "O"* 8 + "P"*2 + "Q"*1 + "R"*6 + "S"*4 + "T"*6 + "U"*4 + "V"*2 + "W"*2 + "X"*1 + "Y"*2 + "Z"*1
-      return letter_string.split("").shuffle
+      @tile_bag = ([?A]*9 + [?B]*2 + [?C]*2 + [?D]*4 + [?E]*12 + [?F]*2 + [?G]*3 + [?H]*2 + [?I]*9 + [?J] +
+      [?K] + [?L]*4 + [?M]*2 + [?N]*6 + [?O]*8 + [?P]*2 + [?Q]*1 + [?R]*6 + [?S]*4 + [?T]*6 + [?U]*4 +
+      [?V]*2 + [?W]*2 + [?X]*1 + [?Y]*2 + [?Z]*1).shuffle
     end
 
     def draw_tiles(num)
