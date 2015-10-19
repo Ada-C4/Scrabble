5853a837b3d1a305405038661d52847258bc2b02
noglows <jessica.noglows@outlook.com>
2015-10-19 10:17:56 -0700

    Fixed dictionary so it has better test coverage

--- lib/word_dictionary.rb
+++ lib/word_dictionary.rb
@@ -7,22 +7,23 @@ class WordDictionary
     @dictionary = Dictionary.from_file('./lib/system_words.txt')
   end
 
+  # We used to use a txt file of words that contained words with hyphens and proper nouns so we had some additional tests, but after switching to Letterpress's word list, we do not need the checks that are commented out.
   def check_word(word)
-    exists = @dictionary.exists?(word)
-    if exists == true
-      if word[0].upcase == word[0]
-        puts "Sorry, words spelled with a capital letter cannot be used."
-        return false
-      elsif word.include? "-"
-        puts "Sorry, words with a hyphen are not accepted."
-        return false
-      elsif word.include? "'"
-        puts "Sorry, words with an apostrophe are not accepted."
-        return false
-      else return true
-      end
-    else
-      return false
-    end
+    exists = @dictionary.exists?(word.downcase)
+    exists == true ? (return true) : (return false)
+      # if word[0].upcase == word[0]
+      #   puts "Sorry, words spelled with a capital letter cannot be used."
+      #   return false
+      # elsif word.include? "-"
+      #   puts "Sorry, words with a hyphen are not accepted."
+      #   return false
+      # elsif word.include? "'"
+      #   puts "Sorry, words with an apostrophe are not accepted."
+      #   return false
+      # else return true
+      # end
+    # else
+    #   return false
+    # end
   end
 end
